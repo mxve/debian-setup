cf3c35197466113ce9db0ab8dd9b01b6db1cb353
Jan-Hendrik Ude <68632137+mxve@users.noreply.github.com>
2021-01-18 19:30:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,6 +19,13 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
+echo -e "\n\n\n\nInstalling rust-mdr\n\n\n\n"
+mkdir -pv /opt/bin/rust-mdr
+cd /opt/bin/rust-mdr
+curl -LJO https://github.com/mxve/rust-mdr/releases/download/v0.1.0-release/rust-mdr
+chmod +x rust-mdr
+ln -s /opt/bin/rust-mdr/rust-mdr /usr/bin/mdr
+
 echo -e "\n\n\n\nSetting timezone Europe/Berlin\n\n\n\n"
 sleep 3
 echo "Europe/Berlin" > /etc/timezone
