c8799bfcb9b3e872591fe72e92d50ff8abdd93e7
Jan-Hendrik Ude <68632137+mxve@users.noreply.github.com>
2021-01-18 16:43:27 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# nano
+## Ctrl-K cuts from cursor to end of line
+## Scroll line by line, not chunk by chunk
+## wrap lines to fit
+## alias ee to nano
+
+alias nano="nano -kS --softwrap"
+alias ee="nano"
+
+# ls
+## Enable colors
+## list format
+## show .files
+## human readable sizes
+
+alias ls="ls -lah --color=auto"
+
+# df & du
+## human readable sizes
+alias df="df -h"
+alias du="du -h"
+
+# ping
+## ping 4 times
+alias ping="ping -c 4"
+
+# grep
+## colors
+alias grep="grep --color=auto"
+
+# gp
+## "grep process", grep from ps ax
+alias gp="ps ax | grep"
+
+# mkdir
+## create parent directories
+## print every directory
+alias mkdir="mkdir -pv"
