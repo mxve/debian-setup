08778b5229ab9a5375bf40e372e3ee3fb086df1e
Jan-Hendrik Ude <68632137+mxve@users.noreply.github.com>
2022-02-10 00:01:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,16 +4,18 @@ echo -e "\n\n\n\nUpdating & installing basic tools\n\n\n\n"
 sleep 3
 apt-get update
 apt-get upgrade -y
-apt-get install -y build-essential goaccess htop nload screen nano debian-goodies unzip zip curl wget git fail2ban rsync python3 python3-pip python3-setuptools cifs-utils logwatch
+apt-get install -y gnupg build-essential goaccess sudo htop nload screen nano debian-goodies unzip zip curl wget git fail2ban rsync python3 python3-pip python3-setuptools cifs-utils logwatch
 
 echo -e "\n\n\n\nInstalling docker\n\n\n\n"
 sleep 3
 apt-get remove -y docker docker-engine docker.io containerd runc docker-compose
 apt-get install -y apt-transport-https ca-certificates gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+ apt-get update
+ apt-get install docker-ce docker-ce-cli containerd.io
 
 echo -e "\n\n\n\nSetting timezone Europe/Berlin\n\n\n\n"
 sleep 3
