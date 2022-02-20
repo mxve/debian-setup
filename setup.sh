5e7873888af8e1eb1049d5b6ea5253379309b5ff
Jan-Hendrik Ude <68632137+mxve@users.noreply.github.com>
2022-02-20 08:17:48 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo -e "\n\n\n\nUpdating & installing basic tools\n\n\n\n"
+echo "Updating & installing basic tools"
 sleep 3
 apt-get update
 apt-get upgrade -y
 apt-get install -y gnupg build-essential goaccess sudo htop nload screen nano debian-goodies unzip zip curl wget git fail2ban rsync python3 python3-pip python3-setuptools cifs-utils logwatch
 
-echo -e "\n\n\n\nInstalling docker\n\n\n\n"
+echo "Installing docker"
 sleep 3
 apt-get remove -y docker docker-engine docker.io containerd runc docker-compose
 apt-get install -y apt-transport-https ca-certificates gnupg-agent software-properties-common
@@ -17,21 +17,26 @@ echo \
  apt-get update
  apt-get install docker-ce docker-ce-cli containerd.io
 
-echo -e "\n\n\n\nSetting timezone Europe/Berlin\n\n\n\n"
+echo "Setting timezone Europe/Berlin"
 sleep 3
 echo "Europe/Berlin" > /etc/timezone
 unlink /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
 
-echo -e "\n\n\n\nCreating /etc/profile.d/mxve_bash.sh\n\n\n\n"
+echo "Creating /etc/profile.d/mxve_bash.sh"
 sleep 3
 curl -fsSL https://raw.githubusercontent.com/mxve/debian-setup/main/profile.d/mxve_bash.sh > /etc/profile.d/mxve_bash.sh
 chmod +x /etc/profile.d/mxve_bash.sh
 
-echo -e "\n\n\n\nReplacing sshd_config\n\n\n\n"
+echo "Replacing sshd_config"
 sleep 10
 # bippity boppity ssh key mandatory
 curl -fsSL https://raw.githubusercontent.com/mxve/debian-setup/main/ssh/sshd_config > /etc/ssh/sshd_config
 systemctl restart sshd
 
-echo -e "\n\n\n\nDone, check ssh functionality before closing session\n\n\n\n"
+echo "Settings up firewall"
+apt install ufw -y
+ufw allow ssh
+ufw enable
+
+echo -e "\n\n\n\nDone, check ssh functionality before closing session"
