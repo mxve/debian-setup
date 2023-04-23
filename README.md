e7997ad7da3387b330c0f69b753834868ee3e452
mxve <68632137+mxve@users.noreply.github.com>
2023-04-23 20:44:33 +0000

    updood

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# debian-setup
-## You shouldn't use this
-
+# You shouldn't use this
 ```wget -O - https://raw.githubusercontent.com/mxve/debian-setup/main/setup.sh | bash```
+
+### install stuff
+- update/upgrade
+- install gnupg, build-essential, sudo, htop, nload, screen, nano, debian-goodies, unzip, zip, curl, wget, git, fail2ban, rsync, cifs-utils, ufw
+
+### download configs
+[.bashrc](.bashrc) to ```/etc/skel/.bashrc```, ```~/.bashrc```
+[ssh_config](ssh_config) to ```/etc/ssh/ssh_config```
+[issue.net](issue.net) to ```/etc/issue.net```
+
+### generate ssh key
--- setup.sh
+++ setup.sh
@@ -1,48 +1,21 @@
 #!/bin/bash
 
-echo "Updating & installing basic tools"
-sleep 3
 apt-get update
 apt-get upgrade -y
-apt-get install -y gnupg build-essential sudo htop nload screen nano debian-goodies unzip zip curl wget git fail2ban rsync python3 python3-pip python3-setuptools cifs-utils logwatch
+apt-get install -y gnupg build-essential sudo htop nload screen nano debian-goodies unzip zip curl wget git fail2ban rsync cifs-utils ufw
 
-echo "Installing goaccess"
-wget -O - https://deb.goaccess.io/gnugpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/goaccess.gpg >/dev/null
-echo "deb [signed-by=/usr/share/keyrings/goaccess.gpg] https://deb.goaccess.io/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/goaccess.list
-apt-get update
-apt-get install goaccess -y
+ufw allow ssh
+ufw enable
 
-echo "Installing docker"
-sleep 3
-apt-get remove -y docker docker-engine docker.io containerd runc docker-compose
-apt-get install -y apt-transport-https ca-certificates gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
- apt-get update
- apt-get install docker-ce docker-ce-cli containerd.io
+curl -fsSL https://raw.githubusercontent.com/mxve/debian-setup/main/.bashrc > /etc/skel/.bashrc
+curl -fsSL https://raw.githubusercontent.com/mxve/debian-setup/main/.bashrc > ~/.bashrc
 
-echo "Setting timezone Europe/Berlin"
-sleep 3
+curl -fsSL https://raw.githubusercontent.com/mxve/debian-setup/main/sshd_config > /etc/ssh/sshd_config
+curl -fsSL https://raw.githubusercontent.com/mxve/debian-setup/main/issue.net > /etc/issue.net
+systemctl restart sshd
+
 echo "Europe/Berlin" > /etc/timezone
 unlink /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
 
-echo "Creating /etc/profile.d/mxve_bash.sh"
-sleep 3
-curl -fsSL https://raw.githubusercontent.com/mxve/debian-setup/main/profile.d/mxve_bash.sh > /etc/profile.d/mxve_bash.sh
-chmod +x /etc/profile.d/mxve_bash.sh
-
-echo "Replacing sshd_config"
-sleep 10
-# bippity boppity ssh key mandatory
-curl -fsSL https://raw.githubusercontent.com/mxve/debian-setup/main/ssh/sshd_config > /etc/ssh/sshd_config
-systemctl restart sshd
-
-echo "Settings up firewall"
-apt install ufw -y
-ufw allow ssh
-ufw enable
-
-echo -e "\n\n\n\nDone, check ssh functionality before closing session"
+ssh-keygen -t rsa -b 4096
