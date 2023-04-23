12a72f82528858e49f4eab700d47ae61edd9e263
mxve <68632137+mxve@users.noreply.github.com>
2023-04-23 20:46:05 +0000

    don't enable ufw by default

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y gnupg build-essential sudo htop nload screen nano debian-goodies unzip zip curl wget git fail2ban rsync cifs-utils ufw
 
-ufw allow ssh
-ufw enable
-
 curl -fsSL https://raw.githubusercontent.com/mxve/debian-setup/main/.bashrc > /etc/skel/.bashrc
 curl -fsSL https://raw.githubusercontent.com/mxve/debian-setup/main/.bashrc > ~/.bashrc
 
